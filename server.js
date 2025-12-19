91b41f6a7bd188261be9e797184e6397fae7a541
jeevasanjaysv <jeevasanjay.s2024ece@sece.ac.in>
2025-12-19 14:55:51 +0530

    condition added in server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import dotenv from "dotenv";
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+
+
 import express from'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
